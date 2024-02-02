3917c4b5f56670e3282f6dd5e628ebcb9f11622c
xiran lin <xiranlin@uw.edu>
2024-02-01 17:08:15 -0800

    111

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sqlite3
-
 import streamlit as st
 from pydantic import BaseModel
 import streamlit_pydantic as sp
+from typing import Literal
 
 # Connect to our database
 con = sqlite3.connect("todoapp.sqlite", isolation_level=None)
@@ -13,8 +13,9 @@ cur.execute(
     """
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY,
-        name TEXT,
+        task TEXT,
         description TEXT,
+        category TEXT,
         is_done BOOLEAN
     )
     """
@@ -22,8 +23,9 @@ cur.execute(
 
 # Define our Form
 class Task(BaseModel):
-    name: str
+    task: str
     description: str
+    category: Literal ['school', 'work', 'personal']
     is_done: bool
 
 # This function will be called when the check mark is toggled, this is called a callback function
@@ -34,6 +36,14 @@ def toggle_is_done(is_done, row):
         """,
         (is_done, row[0]),
     )
+    
+def delete_task(row):
+    cur.execute(
+        """
+        DELETE FROM tasks WHERE id = ?
+        """,
+        (row[0],),
+    )
 
 def main():
     st.title("Todo App")
@@ -43,9 +53,9 @@ def main():
     if data:
         cur.execute(
             """
-            INSERT INTO tasks (name, description, is_done) VALUES (?, ?, ?)
+            INSERT INTO tasks (task, description, category, is_done) VALUES (?, ?, ?, ?)
             """,
-            (data.name, data.description, data.is_done),
+             (data.task, data.description, data.category, data.is_done),
         )
 
     data = cur.execute(
@@ -63,18 +73,26 @@ def main():
     #     )
     #     return
 
-    cols = st.columns(3)
-    cols[0].write("Done?")
-    cols[1].write("Name")
+    cols = st.columns(4)
+    cols[0].write("Status")
+    cols[1].write("Task")
     cols[2].write("Description")
+    cols[3].write("Category")
+
+
     for row in data:
-        cols = st.columns(3)
+        cols = st.columns(4)
+
+
         cols[0].checkbox('is_done', row[3], label_visibility='hidden', key=row[0], on_change=toggle_is_done, args=(not row[3], row))
         cols[1].write(row[1])
         cols[2].write(row[2])
+        cols[3].write(row[3])
         # cols[2].markdown(
         #     f'<a target="_self" href="/?id=123" style="display: inline-block; padding: 6px 10px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 12px; border-radius: 4px;">Action Text on Button</a>',
         #     unsafe_allow_html=True,
         # )
 
+
 main()
+
